94d2dafdc249e7b7a44fcb0391744d1316a3cea1
wxu <wxuonline@gmail.com>
2018-03-09 18:03:56 +0800

    2018-03-09 下午

--- src/app/web/web-routing.modult.ts
+++ src/app/web/web-routing.modult.ts
@@ -1,15 +1,15 @@
 import { NgModule }             from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-// import { WebArticleComponent } from './article.component';
+import { WebListComponent } from './list.component';
 
 const articleRoutes: Routes = [{
     path: '',
-    // component: WebArticleComponent,
+    // component: WebListComponent,
     children: [{
         path: '',
         children: [
-            // { path: '', component: WebArticleComponent }
+            // { path: '', component: WebListComponent }
             {
                 path: 'resume2018',
                 loadChildren: 'app/resume/resume.module#ResumeModule',
@@ -18,9 +18,40 @@ const articleRoutes: Routes = [{
     }]
 }]
 
+const webRoutes: Routes = [
+    {
+      path: '',
+      redirectTo: '/webs',
+      pathMatch: 'full'
+    },
+    {
+    path: '/webs',
+    component: WebListComponent,
+    children: [
+      // { path: 'profile', component: ProfileSettingsComponent }
+    ]
+  },
+    // {
+    //     path: 'blogs',
+    //     component: BlogsListComponent
+    // },
+    // {
+    //     path: 'webs',
+    //     component: WebListComponent
+    // },
+    // {
+    //     path: 'webs/:route',
+    //     loadChildren: 'app/web/scheduling.module#WebSchedulingModule',
+    //     // component: WebArticleComponent
+    // },
+    // {
+    //     path: 'resume2018',
+    //     loadChildren: 'app/resume/resume.module#ResumeModule',
+    // }
+];
 @NgModule({
     imports: [
-        RouterModule.forChild(articleRoutes)
+        RouterModule.forChild(webRoutes)
     ],
     exports: [ RouterModule ]
 })
